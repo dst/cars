51ca4536c7449e175392129872f7fa1a2d746b1f
Dariusz Stefanski <d.a.stefanski@gmail.com>
2015-10-31 21:19:43 +0100

    Use google-chrome to display reports

--- tools/open-reports.sh
+++ tools/open-reports.sh
@@ -5,7 +5,7 @@
 # Author: Dariusz Stefanski
 
 REPORTS_DIR=`pwd`/"../build/reports"
-BROWSER="firefox"
+BROWSER="google-chrome"
 
 for report in /jacoco/test/html/index.html /tests/index.html; do
     ${BROWSER} ${REPORTS_DIR}/${report}
